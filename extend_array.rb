dc1870dbc3c7fda3c870e1fa593a5301db2a7c8d
Ancalima Silme <ancalimasilme@gmail.com>
2014-10-18 16:46:32 +0700

    Refactoring INSERT_ARRAY methods

--- extend_array.rb
+++ extend_array.rb
@@ -1,26 +1,20 @@
 # coding: utf-8
 
 class Array
-  def insert_array(position, array)
-    index = 0
-    new_array = self.clone
 
-    array.each do |element|
-      new_array.insert position, element
-      position += 1
+  # INSERT_ARRAY methods
+  ["insert_array", "insert_array!"].each do |method_name|
+    define_method method_name do |position, additional_array|
+      index = 0
+      array = (method_name.include? "!") ? self : self.clone
+
+      additional_array.each do |element|
+        array.insert position, element
+        position += 1
+      end
+
+      array
     end
-
-    new_array
   end
 
-  def insert_array!(position, array)
-    index = 0
-
-    array.each do |element|
-      self.insert position, element
-      position += 1
-    end
-
-    self
-  end
 end
