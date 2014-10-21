870175df45a00ee53db6638330535cc2b195f328
Ancalima Silme <ancalimasilme@gmail.com>
2014-10-21 21:43:24 +0700

    Added new method for Array: contains? Wrote tests for it.

--- extend_array.rb
+++ extend_array.rb
@@ -2,7 +2,7 @@
 
 class Array
 
-  # INSERT_ARRAY methods
+  # INSERT_ARRAY and INSERT_ARRAY!
   ["insert_array", "insert_array!"].each do |method_name|
     define_method method_name do |position, additional_array|
       index = 0
@@ -15,6 +15,17 @@ class Array
 
       array
     end
+  end
+
+  # CONTAINS?
+  def contains?(compare_variable)
+    raise ArgumentError, "Argument may be just Array." if compare_variable.class.to_s != "Array"
+    raise ArgumentError, "Argument may be just Array, but not empty." if compare_variable.class.to_s == "Array" && compare_variable.empty?
+
+    working_array = self.compact.uniq
+    result = compare_variable.map { |element| working_array.include? element  }.uniq
+
+    result.size == 1 && result.first == true
   end
 
 end
--- spec/extend_array_spec.rb
+++ spec/extend_array_spec.rb
@@ -5,46 +5,74 @@ require_relative "../extend_array"
 
 describe Array do
 
-  before :all do
-    @original_array = [1, 2, 3, 7, 9, 10]
-    @addition_array = [4, 5, 6]
-    @position = 3
-  end
+  describe "#insert_array"  do
 
-  context "#insert_array" do
     before :all do
-      @result_array = @original_array.insert_array @position, @addition_array
+      @original_array = [1, 2, 3, 7, 9, 10]
+      @addition_array = [4, 5, 6]
+      @position = 3
     end
 
-    it "количество элементов должно быть равно сумме изначального массива и вкладываемого" do
-      expect(@result_array.size).to eql(@original_array.size + @addition_array.size)
+    context do
+      before :all do
+        @result_array = @original_array.insert_array @position, @addition_array
+      end
+
+      it "количество элементов должно быть равно сумме изначального массива и вкладываемого" do
+        expect(@result_array.size).to eql(@original_array.size + @addition_array.size)
+      end
+
+      it "в выходном массиве должны быть все элементы из изначального массива" do
+        expect(@result_array - @addition_array).to eql @original_array
+     end
+
+      it "в выходном массиве должны быть все элементы из вкладываемого массива" do
+        expect(@result_array - @original_array).to eql @addition_array
+      end
     end
 
-    it "в выходном массиве должны быть все элементы из изначального массива" do
-      expect(@result_array - @addition_array).to eql @original_array
-   end
+    context "!" do
+      before :all do
+        @original_array_before_changes = @original_array.clone
+        @original_array.insert_array! @position, @addition_array
+      end
 
-    it "в выходном массиве должны быть все элементы из вкладываемого массива" do
-      expect(@result_array - @original_array).to eql @addition_array
+      it "количество элементов оригинального массива должно увеличится на количество вкладываемого" do
+        expect(@original_array.size - @addition_array.size).to eql @original_array_before_changes.size
+      end
+
+      it "оригинальный массив содержит все свои элементы" do
+        expect(@original_array - @addition_array).to eql @original_array_before_changes
+      end
+
+      it "оригинальный массив содержит все элементы вложенного массива" do
+        expect(@original_array - @original_array_before_changes).to eql @addition_array
+      end
     end
   end
 
-  context "#insert_array!" do
+
+
+  context "#contains?" do
     before :all do
-      @original_array_before_changes = @original_array.clone
-      @original_array.insert_array! @position, @addition_array
+      @original_array = [1, 2, "", "test string", 87, {}, "f", 2, 15, 3, 5.3]
     end
 
-    it "количество элементов оригинального массива должно увеличится на количество вкладываемого" do
-      expect(@original_array.size - @addition_array.size).to eql @original_array_before_changes.size
+    it "возвращает ошибку, если параметром передан не массив" do
+      expect { @original_array.contains? 87 }.to raise_error(ArgumentError, "Argument may be just Array.")
     end
 
-    it "оригинальный массив содержит все свои элементы" do
-      expect(@original_array - @addition_array).to eql @original_array_before_changes
+    it "возвращает ошибку, если параметром передан пустой массив" do
+      expect { @original_array.contains? [] }.to raise_error(ArgumentError, "Argument may be just Array, but not empty.")
     end
 
-    it "оригинальный массив содержит все элементы вложенного массива" do
-      expect(@original_array - @original_array_before_changes).to eql @addition_array
+    it "true" do
+      expect(@original_array.contains? [87, 2, "f", 5.3]).to be_truthy
+    end
+
+    it "false" do
+      expect(@original_array.contains? [8, 2, "h", 5.3]).to be_falsey
     end
   end
+
 end
